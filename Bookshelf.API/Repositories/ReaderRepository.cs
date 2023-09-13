cf3b70c92ae35ed3846d159231ab864228cd9055
Davi <davifrancisco214@outlook.com>
2023-09-13 07:55:30 -0300

    Adicionando métodos para obter o leitor e atualizar sua senha

--- Bookshelf.API/Repositories/ReaderRepository.cs
+++ Bookshelf.API/Repositories/ReaderRepository.cs
@@ -35,5 +35,28 @@ namespace Bookshelf.API.Repositories
 
             return reader;
         }
+
+        public async Task<Reader> GetReaderAsync([FromServices] BookshelfDbContext context,
+            string userName)
+        {
+            var reader = await context.Readers.FirstOrDefaultAsync(x => x.UserName == userName);
+
+            return reader;
+        }
+
+        public async Task<bool> UpdatePasswordReader([FromServices] BookshelfDbContext context,
+            ChangePasswordReaderViewModel viewModel)
+        {
+            var reader = await context.Readers.FirstOrDefaultAsync(x => x.UserName == viewModel.UserName);
+
+            if (reader == null)
+                return false;
+
+            reader.Password = viewModel.NewPassword;
+            context.Readers.Update(reader);
+            await context.SaveChangesAsync();
+
+            return true;
+        }
     }
 }
