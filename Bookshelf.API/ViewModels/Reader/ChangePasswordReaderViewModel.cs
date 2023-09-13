ea274dd7f53bebcd85c0a985d83e83e8010c2a40
Davi <davifrancisco214@outlook.com>
2023-09-13 07:53:08 -0300

    Adicionando o ViewModel para alterar a senha do leitor

---
+++ Bookshelf.API/ViewModels/Reader/ChangePasswordReaderViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Bookshelf.API.ViewModels.Reader;
+
+public class ChangePasswordReaderViewModel
+{
+    public string UserName { get; set; }
+    public string NewPassword { get; set; }
+}
