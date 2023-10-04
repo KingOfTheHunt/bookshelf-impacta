018e07f0378e10ef4886d89e34e21465e594e479
Davi <davifrancisco214@outlook.com>
2023-10-04 09:30:50 -0300

    Criando o Controlador e a view do livro

---
+++ Bookshelf.Web/Controllers/BookController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bookshelf.Web.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index([FromQuery] string query)
+        {
+            ViewData["Title"] = query;
+            return View();
+        }
+    }
+}
