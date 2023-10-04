a99244cdf2512fed407ab7ce9a2abb7dfec9265e
Davi <davifrancisco214@outlook.com>
2023-10-04 08:33:26 -0300

    Pesquisa de livros

---
+++ Bookshelf.API/Controllers/BookController.cs
@@ -0,0 +1,19 @@
+﻿using Bookshelf.API.Data;
+using Bookshelf.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookshelf.API.Controllers
+{
+    [Route("v1/books/")]
+    public class BookController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetBooks([FromServices] BookshelfDbContext context,
+            [FromServices] BookRepository repository)
+        {
+            var books = await repository.GetBooksAsync(context);
+
+            return Ok(books);
+        }
+    }
+}
---
+++ Bookshelf.API/Repositories/BookRepository.cs
@@ -0,0 +1,28 @@
+﻿using Bookshelf.API.Data;
+using Bookshelf.API.Models;
+using Bookshelf.API.ViewModels.Book;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Bookshelf.API.Repositories;
+
+public class BookRepository
+{
+    public async Task<List<BookSearchViewModel>> GetBooksAsync([FromServices] BookshelfDbContext context)
+    {
+        return await context.Books.Include(x => x.Authors)
+            .Include(x => x.Genres)
+            .AsSplitQuery()
+            .Where(x => x.Id == 2 || x.Id == 4 || x.Id == 6)
+            .Select<Book, BookSearchViewModel>(x => new BookSearchViewModel
+            {
+                Title = x.Title,
+                Subtitle = x.Subtitle,
+                Authors = x.Authors.Select(y => y.Name).ToList(),
+                Genres = x.Genres.Select(y => y.Name).ToList()
+            })
+            .AsNoTracking()
+            .ToListAsync();
+    }
+}
