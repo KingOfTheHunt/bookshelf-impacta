468422ce5064aa4081a3d884de687af6fcfa09cb
Davi <davifrancisco214@outlook.com>
2023-10-04 08:31:26 -0300

    Criando uma view model para o livro

---
+++ Bookshelf.API/ViewModels/Book/BookSearchViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Bookshelf.API.ViewModels.Book;
+
+public class BookSearchViewModel
+{
+    public string Title { get; set; }
+    public string? Subtitle { get; set; }
+    public List<string> Authors { get; set; }
+    public List<string> Genres { get; set; }
+}
