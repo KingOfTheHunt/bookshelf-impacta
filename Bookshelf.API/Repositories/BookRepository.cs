c29319105c8f722f19cb25a3baa1594dabfa66a1
Davi <davifrancisco214@outlook.com>
2023-10-12 20:23:33 -0300

    Concluindo a pesquisa de livros

--- Bookshelf.API/Repositories/BookRepository.cs
+++ Bookshelf.API/Repositories/BookRepository.cs
@@ -3,27 +3,60 @@ using Bookshelf.API.Models;
 using Bookshelf.API.ViewModels.Book;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Reflection.Metadata.Ecma335;
+using Dapper;
+using Microsoft.Data.SqlClient;
 
 namespace Bookshelf.API.Repositories;
 
 public class BookRepository
 {
-    public async Task<List<BookSearchViewModel>> GetBooksAsync([FromServices] BookshelfDbContext context, string query)
+    public async Task<IEnumerable<BookSearchViewModel>> GetBooksAsync([FromServices] BookshelfDbContext context,
+        string query)
     {
-        return await context.Books.Include(x => x.Authors)
-            .Include(x => x.Genres)
-            .AsSplitQuery()
-            .Where(x => x.Title.Contains(query) || x.ISBN == query)
-            .Select<Book, BookSearchViewModel>(x => new BookSearchViewModel
+        using var connection = new SqlConnection(context.Database.GetConnectionString());
+
+        string sql = @"SELECT [Book].[Id], [Book].[Title], [Book].[Subtitle],
+                [Author].[Name], [Genre].[Name] FROM [Book]
+                INNER JOIN [BookAuthor] ON [BookAuthor].[BookId] = [Book].[Id]
+                INNER JOIN [Author] ON [BookAuthor].[AuthorId] = [Author].[Id]
+                INNER JOIN [BookGenre] ON [BookGenre].[BookId] = [Book].[Id]
+                INNER JOIN [Genre] ON [BookGenre].[GenreId] = [Genre].[Id]
+                WHERE [Book].[Title] LIKE @likeQuery OR [Book].[ISBN] = @query
+                OR [Author].[Name] LIKE @likeQuery";
+
+        var searchedBooks = new List<BookSearchViewModel>();
+
+        var result = await connection.QueryAsync<Book, Author, Genre, BookSearchViewModel>(
+            sql, (book, author, genre) =>
             {
-                Id = x.Id,
-                Title = x.Title,
-                Subtitle = x.Subtitle,
-                Authors = x.Authors.Select(y => y.Name).ToList(),
-                Genres = x.Genres.Select(y => y.Name).ToList()
-            })
-            .AsNoTracking()
-            .ToListAsync();
+                var searchedBook = searchedBooks.FirstOrDefault(x => x.Id ==  book.Id);
+
+                if (searchedBook == null)
+                {
+                    searchedBook = new()
+                    {
+                        Id = book.Id,
+                        Title = book.Title,
+                        Subtitle = book.Subtitle,
+                    };
+                    searchedBook.Authors.Add(author.Name);
+                    searchedBook.Genres.Add(genre.Name);
+
+                    searchedBooks.Add(searchedBook);
+                }
+                else
+                {
+                    if (searchedBook.Authors.Any(x => x == author.Name) == false)
+                        searchedBook.Authors.Add(author.Name);
+                    
+                    searchedBook.Genres.Add(genre.Name);
+                }
+
+                return searchedBook;
+            },
+            new { LikeQuery = $"%{query}%", query },
+            splitOn: "Name,Name");
+
+        return searchedBooks;
     }
 }
