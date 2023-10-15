d673ffbc0eb67915e3002959ddd047e6f88fbabc
Davi <davifrancisco214@outlook.com>
2023-10-15 17:43:30 -0300

    Armazenando e recuperando os valores na Session

---
+++ Bookshelf.Web/Extensions/HttpContextExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace Bookshelf.Web.Extensions;
+
+public static class HttpContextExtension
+{
+    public static string? GetValueFromSession(this HttpContext httpContext, string key) =>
+        httpContext.Session.GetString(key);
+
+    public static void SetValueInSession(this HttpContext httpContext, string key, string value) =>
+        httpContext.Session.SetString(key, value);
+
+    public static void ClearSession(this HttpContext httpContext) => httpContext.Session.Clear(); 
+}
